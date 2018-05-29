dcfb29d0329fb9b1a8537fe986732124c9655167
dnicolaou <demetri.nicolaou@gmail.com>
2018-05-28 18:47:32 -0700

    Added function parse(gen)
    parses genotypes into phases

--- create_phases.py
+++ create_phases.py
@@ -11,21 +11,18 @@ def parse(head, tail):
 			parse(head+'1', tail[1:])
 			parse(head+'0', tail[1:])
 
-
-def main():
-	gen = '12100'
+def phase(gen):
 	parse('', gen)
-	print('possible haplotypes: ')
-	print(haps)
-	
-
 	phases = []
 	for i in range(0, (int)(len(haps)/2)):
 		phases.append((haps[i], haps[-(i+1)]))
+	return phases
 
-	print('phases:')
-	print(phases)
 
+
+def main():
+	gen = '12100'
+	print(phase(gen))
 
 if __name__ == "__main__":
 	main()
